42a01e1eee635e2f8d217502c1aaa806e3fa8128
Navit Choudhary <navitchoudhary22@gmail.com>
2021-08-05 15:01:28 +1000

    added mongo db

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,20 +2,36 @@ const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
 
+const addProjectToApp = (project) => {
+    $.ajax({
+        url: '/api/projects',
+        data: project,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
+    })
+}
+
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
 
     console.log("Form Data Submitted: ", formData);
+    addProjectToApp(formData);
 }
 
 const getProjects = () => {
     $.get('/api/projects',(response) => {
         if(response.statusCode==200){
             addCards(response.data);
+        }
+        else {
+            console.log(response)
         }
     })
 }
@@ -28,7 +44,7 @@ const addCards = (items) => {
     '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
     '<div class="card-reveal">'+
         '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
+        '<p class="card-text">'+item.description+'</p>'+
       '</div></div></div>';
       $("#card-section").append(itemToAppend)
     });
